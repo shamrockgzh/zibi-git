2ab18fb385b63e846409ff4e2bee6d8c9cdcc1c1
gaozhonghua <1085755854@qq.com>
2016-12-09 18:49:10 +0800

    登录注册

--- app.py
+++ app.py
@@ -2,9 +2,16 @@
 
 from flask import Flask, abort, jsonify, render_template, request, make_response, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
-from main.misc.errors import ViewProcessJump
 from cerberus import Validator
 import os
+from functools import wraps
+from flask import request, jsonify, g
+from cerberus import Validator
+
+API_FAIL_CODES = {}
+API_FAIL_CODES['SIGN_UP'] = {
+    'USER_NAME_EXISTED': 1,
+}
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -76,12 +83,71 @@ class UserOrdinary(db.Model):
 
 #机构用户
 
+
+#-------------------------------------errors.py-----------------------------------------
+class ViewProcessJump(Exception):
+    def __init__(self, code, extra=None):
+        self.code = code
+        self.extra = extra
+#------------------------------------errors.py-----------------------------------------
+
+#------------------------------------auth.py-------------------------------------------
+
+#拦截器,面向方向编程
+
+
+
+#验证token格式,
+def auth_required(f):
+    @wraps(f)
+    def wrapper(*args,**kwargs):
+        token = request.headers.get('Token')
+        document = {'Token':token}
+        #验证器
+        validator = Validator({
+            'Token': {'type': 'string', 'minlength': 1},
+        })
+
+        #token 为空
+        if token is None:
+            resp_data = {
+                'status': 'fail',
+                'data': {
+                    'message': 'NO_TOKEN',
+                    'code': 0
+                },
+            }
+            return jsonify(**resp_data)
+        #token 格式错误
+        if not validator.validate(document):
+            resp_data = {
+                'status': 'fail',
+                'data': {
+                    'message': 'TOKEN_ERROR',
+                    'code':1
+                },
+            }
+            return jsonify(**resp_data)
+        #无此用户
+        user = UserOrdinary.query.get(int(token))
+        if not user:
+            resp_data = {
+                'status': 'fail',
+                'data': {
+                    'message': 'NOT_FIND_USER',
+                    'code': 2,
+                },
+            }
+            return jsonify(**resp_data)
+
+        g.user = user
+        return  f(*args,**kwargs)
+    return wrapper
+#-------------------------------------------------auth.py-----------------------------
+
+
 #------------------------------------注册登录-------------------------------------------
 
-
-API_FAIL_CODES['SIGN_UP'] = {
-    'USER_NAME_EXISTED': 1,
-}
 
 
 #注册页面
@@ -126,12 +192,12 @@ def sign_up_api():
             password = validator.document.get('password')
             gender = validator.document.get('gender')
 
-            user_existed = User.query.filter(db.text('name = :temp')).params(temp=name).first()
+            user_existed = UserOrdinary.query.filter(db.text('name = :temp')).params(temp=name).first()
 
             if user_existed:
                 raise ViewProcessJump(code='USER_NAME_EXISTED')
 
-            user = User()
+            user = UserOrdinary()
             db.session.add(user)
             user.name = name
             user.password = password
@@ -203,7 +269,7 @@ def sign_in_api():
             name = validator.document.get('name')
             password = validator.document.get('password')
 
-            user_existed = User.query.filter(db.text('name = :temp')).params(temp=name).first()
+            user_existed = UserOrdinary.query.filter(db.text('name = :temp')).params(temp=name).first()
 
             if user_existed is None:
                 raise ViewProcessJump(code='USER_NAME_UNEXISTED')
@@ -243,7 +309,7 @@ def sign_in_api():
 #登陆成功
 @app.route('/sign-in/success/')
 def sign_in_success():
-        return render_template('success.html')
+        return render_template('project.html')
 
 
 
